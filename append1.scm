ba48d0de4bd514c4b195f161f97b54697ad2e5ba
AJ Trantham <ajtrantham@u.boisestate.edu>
2019-09-18 23:55:34 -0600

    Working version but odd implementation. I seem to be getting the coreect results. Copy could still be incorrect and I am making up for it with a append?

--- append1.scm
+++ append1.scm
@@ -7,7 +7,8 @@
 (define (append a count)
       (if (= count 0)
 	(copy a)
-     (cons  (cons (car a) (copy (append (copy a) (- count 1)))) (cdr a))))
+     (cons (cons (car a) (copy (append (copy a) (- count 1)))) (cdr a)))
+)
 
 (display (append '(a b c) 2))
 (display "\n")
